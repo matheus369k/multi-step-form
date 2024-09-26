80ae44ee29d580c6c7a40af939f34dfc9f1c513c
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-09-26 13:22:11 -0300

    resolver error on the response mode

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -12,11 +12,9 @@ export function RootProvider({ children }: { children: React.ReactNode }) {
       <Suspense fallback={<Loading />}>
         <main className="flex flex-col w-full lg:w-auto lg:bg-zinc-50 lg:p-4 font-[family-name:var(--font-Ubuntu-Regular)] lg:rounded-2xl lg:shadow lg:flex-row">
           <ListOfSteps />
-          <Suspense fallback={<Loading />}>
-            <div className="max-w-[620px] lg:w-[620px] min-h-[420px] lg:h-[500px] mx-auto text-blue-950 px-4 sm:px-12 lg:px-20 rounded-lg flex flex-col gap-8 -mt-24 lg:mt-10 mb-2 bg-zinc-50">
-              {children}
-            </div>
-          </Suspense>
+          <div className="max-w-[620px] w-full lg:w-[620px] min-h-[420px] lg:h-[500px] mx-auto text-blue-950 px-4 sm:px-12 lg:px-20 rounded-lg flex flex-col gap-8 -mt-24 lg:mt-10 mb-2 bg-zinc-50">
+            <Suspense fallback={<Loading />}>{children}</Suspense>
+          </div>
         </main>
       </Suspense>
     </Provider>
